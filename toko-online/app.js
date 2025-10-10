24d741bc745d1cecd1ed2bc75b789a59c9c3b269
mayanggsarinr <mayanggsarinr.22@gmail.com>
2025-10-10 17:09:14 +0700

    mongodb model

--- toko-online/app.js
+++ toko-online/app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+//panggil file koneksi database
+require('../app_toko_online/models/db');
 
 //perbaikan ke2
 var indexRouter = require('../toko-online/app_toko_online/routes/index');
